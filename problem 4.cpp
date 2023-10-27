a4cd4be724651e62839f80b71eaac8bcd619b052
rakhimovafazilat <147136940+rakhimovafazilat@users.noreply.github.com>
2023-10-27 14:10:01 +0500

    Create problem 4.cpp

---
+++ problem 4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, digit, sum = 0;
+    cout << "Enter an integer number: ";
+    cin >> num;
+
+    int originalNum = num;
+
+    while (num != 0) {
+        digit = num % 10;//6 // 4
+        sum += digit; //0 += 6 sum=6+4
+        num /= 10;// 4 
+    }
+
+    cout << "Sum of digits: " << sum << std::endl;
+    
+    if (sum % 5 == 0) {
+        cout << "Yes";
+    } else {
+    cout << "No";
+    }
+
+    return 0;
+}
