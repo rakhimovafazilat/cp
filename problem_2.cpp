6b20e5ff23759c987bb3fb81e85d4868e49705f2
rakhimovafazilat <147136940+rakhimovafazilat@users.noreply.github.com>
2023-10-27 17:26:48 +0500

    Update problem_2.cpp

--- problem_2.cpp
+++ problem_2.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 
+bool fazilat(int a, int b, int c) {
+    if (a != b && b != c && a != c) { 
+        if (a * a + b * b == c * c || b * b + c * c == a * a||  a * a + c * c == b * b) {
+            return true; 
+        }
+    }
+    return false; 
+}
 
 int main() {
-    
-    int num1;
-    int oddSum = 0;
-    int evenSum = 0;
-    
-  std::cin >> num1;
-    
-    for(int i=0;i<=num1; i++){
-        if(i%2==0){
-            evenSum += i;
-        }else if(i%2==1){
-            oddSum +=i;
-        }
-       
-    } std::cout << oddSum  << " " << evenSum;
-      
+    int a, b, c;
+    std::cin >> a;
+    std::cin >> b;
+    std::cin >> c;
+
+    if (fazilat(a, b, c)) {
+        std::cout << "Yes";
+    } else {
+        std::cout << "No";
+    }
 
     return 0;
 }
