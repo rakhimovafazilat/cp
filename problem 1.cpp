02d0da68413648c7de4a1485020747594fc66b77
rakhimovafazilat <147136940+rakhimovafazilat@users.noreply.github.com>
2023-10-27 15:52:48 +0500

    Update problem 1.cpp

--- problem 1.cpp
+++ problem 1.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main() {
     int n;
-    cout << "Enter a value for n: ";
+   
     cin >> n;
 
-    cout << "Odd numbers from 1 to " << n << ": ";
+    cout  << n << ": ";
     for (int i = 1; i <= n; i++){
         if(i%2==1){
             cout << i << " " ;
         }
-    }  cout << "\nEven numbers from 1 to " << n << ": ";
+    }  cout  << n << ": ";
     for (int i = 2; i <= n; i++) {
         if(i%2==0){
              cout << i << " ";
